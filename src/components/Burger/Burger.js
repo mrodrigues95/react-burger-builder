55af4797b23d1e96444e51939c88aed61e08e7b7
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-07 15:09:10 -0400

    added state and outputting dynamic burger ingredients to the BurgerBuilder container

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,27 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 // This acts as a wrapper around all of the burger ingredients.
 const burger = (props) => {
+  // Map the ingredients object into an array of ingredients.
+  // Each array element will contain the ingredient type plus the quantity (e.g. Salad - 2).
+  // igKey is the ingredient type.
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      }); // array with 2 elements
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []); // flatten the array if props.ingredients is 0.
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
