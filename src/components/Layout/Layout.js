939553411fa8d2b383a97a3ce43fc4142a7a6d8b
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-07 10:51:44 -0400

    added BurgerBuilder state container and styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 // This acts as a wrapper for our core content displayed on the screen.
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
