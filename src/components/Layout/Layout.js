0572f87daa23d75f754929581f6e80fdacec1093
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-08 12:16:32 -0400

    added a responsive side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 // This acts as a wrapper for our core content displayed on the screen.
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
