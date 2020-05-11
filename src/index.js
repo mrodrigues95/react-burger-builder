13a4f13b18dba7629dbdce1587b97781e9843a9f
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-10 21:33:47 -0400

    setup routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+// Enable routing.
+const app = (
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
+
+ReactDOM.render(app, document.getElementById('root'));
