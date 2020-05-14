99275f8b82e3c79d35f01f0d5d353930a4eac5ea
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-14 11:29:33 -0400

    orders are now fetched and managed via redux

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -9,10 +9,10 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_INIT:
-        return {
-            ...state,
-            purchased: false,
-        }
+      return {
+        ...state,
+        purchased: false,
+      };
     case actionTypes.PURCHASE_BURGER_START:
       return {
         ...state,
@@ -30,6 +30,22 @@ const reducer = (state = initialState, action) => {
         orders: state.orders.concat(newOrder),
       };
     case actionTypes.PURCHASE_BURGER_FAIL:
+      return {
+        ...state,
+        loading: false,
+      };
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading: false,
+      };
+    case actionTypes.FETCH_ORDERS_FAIL:
       return {
         ...state,
         loading: false,
