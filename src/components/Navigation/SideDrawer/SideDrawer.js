72b43b086bb649568bbbc10560c8d797da0ca64d
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-14 17:12:52 -0400

    UI state changes depending on user authentication

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -18,7 +18,7 @@ const sideDrawer = (props) => {
       <div className={attachClasses.join(' ')}>
         <Logo height="11%" />
         <nav>
-          <NavigationItems />
+          <NavigationItems isAuthenticated={props.isAuth} />
         </nav>
       </div>
     </Aux>
